056326abe29709f6c0130763b4607844eccb2951
Bernat Marzo <castillejo.marzo.bernat@gmail.com>
2022-05-10 00:33:36 +0200

    chucknorris añadido en el tutorial

--- KnightalesPRO/Assets/Scripts/Tutorial.cs
+++ KnightalesPRO/Assets/Scripts/Tutorial.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
@@ -11,10 +12,13 @@ public class Tutorial : MonoBehaviour
     public GameObject panel3;
     public GameObject panel4;
     public GameObject panel5;
+    public GameObject panel6;
+    public GameObject button;
     public GameObject player;
     public GameObject ghost;
     public Animator animator;
     private bool spawn = false;
+    private bool change = false;
     private List<GameObject> list = new List<GameObject>();
     private int i = 0;
     Stopwatch sp = new Stopwatch();
@@ -23,6 +27,7 @@ public class Tutorial : MonoBehaviour
     Stopwatch sp4 = new Stopwatch();
     Stopwatch sp5 = new Stopwatch();
     Stopwatch sp6 = new Stopwatch();
+    Stopwatch sp7 = new Stopwatch();
     void Start()
     {
         player.transform.position = new Vector3(1.274f, -0.933f, 0.04f);
@@ -30,16 +35,22 @@ public class Tutorial : MonoBehaviour
        
         list.Add(panel3);
         list.Add(panel4);
+        list.Add(panel5);
+
         panel2.SetActive(false);
         panel3.SetActive(false);
         panel4.SetActive(false);
         panel5.SetActive(false);
+        panel6.SetActive(false);
         panel1.SetActive(true);
+
+        button.GetComponent<Button>().interactable = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         
         if (sp.ElapsedMilliseconds > 4000)
         {
@@ -80,7 +91,9 @@ public class Tutorial : MonoBehaviour
             list[i - 1].SetActive(true);
             i++;
             sp5.Start();
+
         }
+
         if (sp5.ElapsedMilliseconds > 2000 && spawn && i == 3)
         {
             sp5.Stop();
@@ -89,29 +102,51 @@ public class Tutorial : MonoBehaviour
             animator.enabled = true;
             list[i - 2].SetActive(false);
             i++;
-            
-        }
 
+        }
         if (ghost == null && i == 4)
         {
-            panel5.SetActive(true);
+            list[i-2].SetActive(true);
             Time.timeScale = 0;
             animator.enabled = false;
             sp6.Start();
             i++;
         }
-
-        if (sp6.ElapsedMilliseconds > 3000 && i > 4)
+        
+        
+        if (sp6.ElapsedMilliseconds > 2000 && spawn && i == 5)
         {
             sp6.Stop();
             sp6.Reset();
-            ChangeScene(1);
+            Time.timeScale = 1;
+            animator.enabled = true;
+            list[i - 3].SetActive(false);
+            i++;
+
         }
 
-
+        if (sp7.ElapsedMilliseconds > 3000 && change)
+        {
+            sp7.Stop();
+            sp7.Reset();
+            ChangeScene(1);
+        }
+        
+        if (player.GetComponent<PlayerMov>().button.active)
+        {
+            button.GetComponent<Button>().interactable = true;
+        }
 
     }
 
+   
+    public void StartGame()
+    {
+        panel6.SetActive(true);
+        button.SetActive(false);
+        sp7.Start();
+        change = true;
+    }
     public void Panel2()
     {
         panel1.SetActive(false);
